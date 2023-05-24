1cc1d2888090ec723e1b72a3c53c32b1998781fe
Kevin Stine <kstine93@gmail.com>
2023-05-24 22:49:43 +0200

    Wrote new tests. Reloaded account information (game reset due to alpha testing; old account was lost). Deleted test file.

--- unit_tests.py
+++ unit_tests.py
@@ -1,28 +1,96 @@
 from src.utilities import *
 from src.agent import *
+from src.contracts import *
+from src.factions import *
+from src.systems import *
+
+from collections import Counter
+from os import listdir,remove,fsdecode
+
+'''
+How to test 'list' commands?
+1. These commands should really be re-named. They're no longer about getting a full list, but rather refreshing the cache.
+2. I think the only measure of success is whether the file in the right folder has been FULLY UPDATED.
+    2a. I can see 'last edited' probably, but that feels weird.
+    2b. What about deleting the files and then re-creating them and seeing if it works?
+        2bi. Since this operation is really intensive, I should probably test infrequently - maybe have a separate test suite.
+'''
 
 
-#----------
-def test_save_agent_metadata_locally():
-    response = {
-         'data':{
-              'token':'test_token'
-              ,'agent':{
-                   'accountId':'test_id'
-                   ,'symbol':'test_callsign'
-              }
-         }
-    }
+#-------
+#UTILITIES
+def assert_equal_dict_keys(data:dict,test_keys:list) -> None:
+    assert Counter(data.keys()) == Counter(test_keys)
 
-    save_agent_metadata_locally(response,local_cfg_filepath="./test_account_info.cfg")
+def empty_directory(dir_path:str):
+    for file in listdir(dir_path):
+        file_path = f"{dir_path}/{fsdecode(file)}"
+        remove(file_path)
 
 
-#----------
-def test_decrypt_and_store_api_key_locally():
-    decrypt_and_store_api_key_locally(local_key_filepath='./test.txt')
+#---------
+#--AGENT--
+#---------
+callsign = "AMBROSIUS-RITZ"
+agent_schema = ['accountId','symbol','headquarters','credits','startingFaction']
+agent_folder = f"./gameData/agents/"
+agent = Agent()
+
+#Reloading local cache
+empty_directory(agent_folder)
+assert_equal_dict_keys(agent.get_agent_details(callsign),agent_schema)
+#Pulling from local cache:
+assert_equal_dict_keys(agent.get_agent_details(callsign),agent_schema)
 
 
-#----------
-if __name__ == "__main__":
-    test_save_agent_metadata_locally()
-    test_decrypt_and_store_api_key_locally()
+#-------------
+#---FACTIONS--
+#-------------
+faction_schema = ["symbol","name","description","headquarters","traits","isRecruiting"]
+faction_filepath = "./gameData/factions/"
+faction = Factions()
+
+#Reloading local cache
+empty_directory(faction_filepath)
+assert_equal_dict_keys(faction.get_faction("VOID"),faction_schema)
+#pulling from local cache:
+assert_equal_dict_keys(faction.get_faction("VOID"),faction_schema)
+
+#TEST_LIST_FACTIONS() !!!
+
+
+#-------------
+#--CONTRACTS--
+#-------------
+contract = Contracts()
+contract_name = "clhz8vpb5132os60d068nfihj"
+contracts_filepath = "./gameData/contracts/"
+contract_schema = ['id','factionSymbol','type','terms','accepted','fulfilled','expiration','deadlineToAccept']
+
+#Reloading local cache
+empty_directory(contracts_filepath)
+assert_equal_dict_keys(contract.get_contract(contract_name),contract_schema)
+#pulling from local cache:
+assert_equal_dict_keys(contract.get_contract(contract_name),contract_schema)
+
+#TEST_LIST_CONTRACTS() !!!
+
+
+#-----------
+#--SYSTEMS--
+#-----------
+system = Systems()
+system_name = "X1-AQ83"
+systems_filepath = "./gameData/systems"
+system_schema = ['symbol','sectorSymbol','type','x','y','waypoints','factions']
+
+#Not emptying systems directory - takes too long to re-populate.
+assert_equal_dict_keys(system.get_system(system_name),system_schema)
+
+#TEST_LIST_SYSTEMS() !!!
+
+
+
+
+print("all tests completed successfully")
+
