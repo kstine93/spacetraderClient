1c8422449391cc9bcedc74fa45c9178e37b29f1c
Kevin Stine <kstine93@gmail.com>
2023-06-26 19:39:52 +0200

    Added new functions to allow player to create new agent at the beginning of the game

--- startup_cli.py
+++ startup_cli.py
@@ -2,8 +2,10 @@
 This separate file before the game initializes is to allow the modification of the chosen player.
 Can also be done manually (less fun) in the gameinfo.yaml file
 """
-from PyInquirer import prompt
-from src.base import SpaceTraderConfigSetup
+# from PyInquirer import prompt
+from simple_term_menu import TerminalMenu
+from src.base import SpaceTraderConfigSetup, RegisterNewAgent
+from src.utilities.custom_types import SpaceFactions
 import subprocess
 
 
@@ -11,30 +13,38 @@ import subprocess
 def set_player() -> None:
     config_setup = SpaceTraderConfigSetup()
     callsigns = config_setup.get_all_callsigns()
-    if len(callsigns) > 1:
-        chosen_callsign = prompt_player_select(callsigns)
-    else:
-        chosen_callsign = callsigns[0]
-    config_setup.set_new_current_agent(chosen_callsign)
+    setup_player_option = "[ Create New Agent ]"
+    callsigns.append(setup_player_option)
+    choice = prompt_player_select(callsigns,"Please pick your player")
+    if choice == setup_player_option:
+        choice = setup_new_player()
 
 #----------
-def prompt_player_select(callsigns:list[str]) -> str:
-    callsigns_list = [{"name":cs} for cs in callsigns]
-    module_list_question = [
-        {
-            'type': 'list',
-            'name': 'Players',
-            'message': 'Please pick your player: ',
-            'choices': callsigns_list,
-        }
-    ]
-    answer_dict = prompt(module_list_question)
-    return list(answer_dict.values())[0]
+def prompt_player_select(callsigns:list[str],title:str) -> str:
+    terminal_menu = TerminalMenu(callsigns,
+                                 title=title,
+                                 menu_cursor_style=("fg_green","bold"),
+                                 menu_highlight_style=None,
+                                 menu_cursor="-> "
+                                )
+    terminal_menu.show()
+    return terminal_menu.chosen_menu_entry
+
+#----------
+def setup_new_player() -> str:
+    print("Welcome new agent! Which name would you like to go by?")
+    id = input("Your name should be in all CAPS and only use letters and hypens.\n> ")
+    faction_list = SpaceFactions._member_names_
+    faction = prompt_player_select(faction_list,f"Welcome {id}! Now please choose your starting faction:")
+    registrant = RegisterNewAgent()
+    registrant.register_new_agent(id,faction)
+    return id
 
 #----------
 if __name__ == "__main__":
+    subprocess.run("clear")
     set_player()
-    subprocess.run(["python3","cli/main.py"])
+    subprocess.run(["python3","cli/main_menu.py"])
 
 
 
