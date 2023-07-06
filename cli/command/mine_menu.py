d1f0e9d6e48eb36daee0f54475fdaee786e89461
Kevin Stine <kstine93@gmail.com>
2023-07-06 15:30:01 +0200

    Created code for 'refine' command Changed command loop code to take in functions to print out borders, etc. Added code to print out a heads-up display (HUD) for command menu. Fixed incorrect descriptions of commands in mine_menu Changed survey and extract commands to handle cases when ship is on cooldown. Some minor variable name changes and type hint changes

--- cli/command/mine_menu.py
+++ cli/command/mine_menu.py
@@ -1,15 +1,29 @@
 
 from src.ship_operator import *
 from src.utilities.basic_utilities import dedup_list
+from src.utilities.custom_types import RefinableProduct
 from cli_utilities import *
 from art.ascii_art import border_med_dash,border_mine_menu
-from art.str_formatting import format_survey_template
+from art.str_formatting import format_survey_template, format_base_hud_template
 
 #==========
 ship_operator:ShipOperator
 
 #==========
-mine_menu_color = "purple" #Color used by default in cli_print
+mine_menu_color = "medium_purple2" #Color used by default in cli_print
+
+#==========
+def print_mine_menu_header() -> str:
+    print_mine_hud()
+    cli_print(border_nav_menu,mine_menu_color)
+
+#==========
+def print_mine_hud() -> str:
+    flightMode = ship_operator.flightMode
+    fuel = ship_operator.fuel
+    system = ship_operator.curr_system
+    hud = format_base_hud_template(flightMode,system,fuel)
+    cli_print(hud)
 
 #==========
 def mine_loop(ship:ShipOperator) -> bool:
@@ -24,7 +38,7 @@ def mine_loop(ship:ShipOperator) -> bool:
     cli_print(border_mine_menu,mine_menu_color)
     cli_print("Choose to mine, survey or refine resources, Captain",mine_menu_color)
 
-    exit = command_loop(mine_menu,sep=border_mine_menu,color=mine_menu_color)
+    exit = command_loop(mine_menu,loop_func=print_mine_menu_header)
     if exit: #If player wants to exit, return True to signal to parent menu
         return True
 
@@ -36,15 +50,15 @@ def mine_loop(ship:ShipOperator) -> bool:
 mine_menu = {
     "survey": {
         "func": lambda: survey(),
-        "desc": "Navigate your ship to a new location."
+        "desc": "Collect information about resources at your current location."
     },
-    "mine": {
+    "extract": {
         "func": lambda: extract(),
-        "desc": "Purchase, sell and study profitable markets"
+        "desc": "Extract resources from the current location\nwith an optional target resource."
     },
     "refine": {
         "func": lambda: refine(),
-        "desc": "Learn more about surrounding ships, waypoints and systems."
+        "desc": "Create a new resource from raw resources in cargo.\nRequires refining module."
     },
     "list": {
         "func": lambda: list_cmds(mine_menu),
@@ -57,14 +71,18 @@ mine_menu = {
 }
 
 #==========
-def survey():
-    cli_print("Surveying waypoint now...",mine_menu_color)
-    ship_operator.survey_waypoint()
+def survey() -> None:
+    cooldown_secs = ship_operator.get_cooldown_seconds()
+    if cooldown_secs > 0:
+        cli_print(f"Cannot survey until cooldown expires in {cooldown_secs} seconds",mine_menu_color)
+    else:
+        cli_print("Surveying waypoint now...",mine_menu_color)
+        ship_operator.survey_waypoint()
     cli_print("Survey list:\n",mine_menu_color)
     list_surveys()
 
 #==========
-def list_surveys():
+def list_surveys() -> None:
     survey_list = ship_operator.surveys
     if len(survey_list) == 0:
         cli_print("No surveys collected yet.",mine_menu_color)
@@ -72,7 +90,7 @@ def list_surveys():
         cli_print(format_survey_template(survey),mine_menu_color)
 
 #==========
-def extract():
+def extract() -> None:
     #QUESTION: Should I filter surveys based on whether they are from the current location?
     #Code will work either way, but we might offer player a surveyed resource not possible at
     #current location...
@@ -82,6 +100,12 @@ def extract():
     #I'm actually considering maybe removing my 'optimal survey' code and just prompting
     #the user to select a survey. It's less sophisticated, but it gives some agency back to the
     #user and lets them make some decisions...
+
+    cooldown_secs = ship_operator.get_cooldown_seconds()
+    if cooldown_secs > 0:
+        cli_print(f"Cannot extract until cooldown expires in {cooldown_secs} seconds",mine_menu_color)
+        return None
+
     menu_items = get_items_in_surveys()
 
     no_preference_option = "Do not target a resource"
@@ -90,9 +114,9 @@ def extract():
     menu_items.append(cancel_option)
 
     #Prompt user to select item to target in mining:
-    wp_menu = create_menu(menu_items,prompt="Try to target a particular surveyed resource?")
+    survey_menu = create_menu(menu_items,prompt="Try to target a particular surveyed resource?")
 
-    chosen_resource = menu_prompt(wp_menu)
+    chosen_resource = menu_prompt(survey_menu)
     if chosen_resource == cancel_option:
         return None
     if chosen_resource == no_preference_option:
@@ -107,7 +131,7 @@ def extract():
     cli_print(f"Extracted {rsc_yield['units']} units of {rsc_yield['symbol']}",mine_menu_color)
 
 #==========
-def get_items_in_surveys():
+def get_items_in_surveys() -> list[str]:
     survey_list = ship_operator.surveys
     rsc_list = []
     for survey in survey_list:
@@ -117,7 +141,23 @@ def get_items_in_surveys():
 
 #==========
 def refine():
-    pass
+    cooldown_secs = ship_operator.get_cooldown_seconds()
+    if cooldown_secs > 0:
+        cli_print(f"Cannot refine until cooldown expires in {cooldown_secs} seconds",mine_menu_color)
+        return None
+
+    cancel_option = "Cancel refining"
+    menu_items = RefinableProduct._member_names_.copy() #Custom type showing all possible values
+    menu_items.append(cancel_option)
+
+    refine_menu = create_menu(menu_items,prompt="Try to target a particular surveyed resource?")
+
+    chosen_resource = menu_prompt(refine_menu)
+    if chosen_resource == cancel_option:
+        return None
+
+    refine_yield = ship_operator.refine(chosen_resource)
+    cli_print(f"Refined {refine_yield['units']} units of {refine_yield['tradeSymbol']}",mine_menu_color)
 
 #==========
 def print_mine_hud():
