c9b6c594dde708e739b60c5a6238a60989507d6c
Kevin Stine <kstine93@gmail.com>
2023-07-19 15:18:48 +0200

    A. Major: changed the way that 'back' and 'exit' commands are implemented. They are now part of local command dictionaries. When keyword return values are given to the command loop, special functionality is triggered (e.g., breaking the command loop). B. As part of restructuring 'back' and 'exit' commands, made a small Enumeration to define these 'CLI command' options. C. Removed 'command_prompt' as a separate function and instead embedded it in the command loop

--- cli/cli_utilities.py
+++ cli/cli_utilities.py
@@ -4,6 +4,13 @@ from rich import print as rprint
 from art.ascii_art import *
 from simple_term_menu import TerminalMenu
 from typing import Callable
+from enum import Enum
+
+#==========
+class CliCommand(Enum):
+    #Commands that are used internally to the command structures of the CLI
+    exit = 1
+    back = 2
 
 #==========
 def cli_clear() -> None:
@@ -22,7 +29,9 @@ def cli_print(string:str,color:str|None=None) -> None:
         typer.echo(string)
 
 #==========
-def create_menu(menu_items:list[str],prompt:str='Choose your next action, Captain.',**kwargs) -> TerminalMenu:
+def create_menu(menu_items:list[str],
+                prompt:str='Choose your next action, Captain.',
+                **kwargs) -> TerminalMenu:
     terminal_menu = TerminalMenu(menu_items,
                                  title=prompt,
                                  menu_cursor_style=("fg_green","bold"),
@@ -33,10 +42,10 @@ def create_menu(menu_items:list[str],prompt:str='Choose your next action, Captai
     return terminal_menu
 
 #==========
-def use_menu(menu_dict:dict,**kwargs):
+def use_menu(menu_dict:dict,**kwargs) -> CliCommand | None:
     menu = create_menu(list(menu_dict.keys()),**kwargs)
     cmd = menu_prompt(menu)
-    command_switch(cmd,menu_dict)
+    return command_switch(cmd,menu_dict)
 
 #==========
 def menu_prompt(menu:TerminalMenu,index:bool=False) -> str:
@@ -44,17 +53,13 @@ def menu_prompt(menu:TerminalMenu,index:bool=False) -> str:
     return menu.chosen_menu_index if index else menu.chosen_menu_entry
 
 #==========
-def command_prompt(prompt:str="Use 'list' or 'menu' to get help. '<-back' for last menu.") -> str:
-    cli_print(prompt)
-    return typer.prompt("=")
-
-
-#==========
 def print_generic_header() -> str:
     cli_print(border_med_equals,"white")
 
 #==========
-def command_loop(cmd_list:dict,prompt:str|None=None,loop_func:Callable=print_generic_header) -> bool:
+def command_loop(cmd_list:dict,
+                 prompt:str|None=None,
+                 loop_func:Callable=print_generic_header) -> CliCommand | None:
     """Generic function to loop indefinitely while processing user commands. The given cmd_list
     provides a standardized mapping of strings with commands (e.g., "{'nav':Navigate_Ship()}").
     Returns a bool flag only to show whether the command was given to exit the game (True).
@@ -64,34 +69,34 @@ def command_loop(cmd_list:dict,prompt:str|None=None,loop_func:Callable=print_gen
     above prompts (e.g., a heads-up display)
     """
     while True:
-        command = command_prompt(prompt) if prompt else command_prompt()
-        if command == "<-back": #Return to previous command loop
-            break
-        if command == "exit": #Return flag showing that all parent command loops should end
-            return True
+        if prompt:
+            cli_print(prompt)
+        command = input("-> ")
         try:
             cli_clear()
             loop_func()
             res = command_switch(command,cmd_list)
-            if res == True: #If response is True, it means an exit command has been given.
-                return True
+            if res == "exit":
+                return res
+            if res == "back":
+                break
         except SystemExit as e:
             if str(e) != "0":
                 typer.echo(f"Error: {e}")
-    return False
+    return None
 
 #==========
-def command_switch(cmd:str,cmd_map:dict) -> bool | None:
+def command_switch(cmd:str,cmd_map:dict) -> CliCommand | None:
     if cmd in cmd_map.keys():
         return cmd_map[cmd].get('func')()
     else:
         cli_print(f"Command '{cmd}' not found","red")
         cli_print(border_med_equals,"red")
-    return False
+    return None
 
 #==========
-def list_cmds(menu_dict:dict):
-    # cli_clear()
+def list_cmds(menu_dict:dict) -> None:
     for (key,val) in menu_dict.items():
-        cli_print(f"== '{key}' ==","orange1")
+        cli_print(f"-> {key}","orange1")
         cli_print(str(val['desc']))
+    cli_print(border_med_dash,"orange1")
