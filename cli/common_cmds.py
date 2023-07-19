e7da633ad1a27a203ae9960d61440adcf413fd50
Kevin Stine <kstine93@gmail.com>
2023-07-19 15:10:56 +0200

    Major: Restructured file to act almost like another menu - but without a command loop. Now usable as an interactive sub-menu from other menus to query ship details.

--- cli/common_cmds.py
+++ cli/common_cmds.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
-from cli_utilities import cli_print
+from typing import Callable
+from cli_utilities import cli_print, cli_clear, use_menu
 from src.contracts import Contracts
-from src.ships import Ships
 from src.ship_operator import ShipOperator
 from art.str_formatting import (format_base_hud_template,
                                 format_contract_list,
@@ -14,54 +14,119 @@ from art.str_formatting import (format_base_hud_template,
                                 format_ship_modules_info_template
 )
 
+#==========
+ship_operator:ShipOperator
+
+#==========
+info_menu_color = "medium_purple2" #Color used by default in cli_print
+
+#==========
+def info_loop(ship:ShipOperator,headerFunc:Callable) -> bool:
+    """Displays information menu for printing info on ship systems.
+    As opposed to other loops in other menus, this menu is not intended to be lingered on - it is
+    instead intended to emulate the previous menu that players were on. For that reason, the
+    provided 'headerFunc' shows the same header as the previous menu and as soon as players make a
+    selection from the menu below, they are returned to that previous menu.
+    """
+    cli_clear()
+
+    global ship_operator
+    ship_operator = ship
+
+    headerFunc()
+    use_menu(info_menu)
+
+    return False
+
 #=========
 #== HUD ==
 #=========
 def print_hud(ship_operator:ShipOperator) -> str:
+    shipName = deepcopy(ship_operator.spaceship_name)
     flightMode = deepcopy(ship_operator.flightMode)
     fuel = deepcopy(ship_operator.fuel)
     system = deepcopy(ship_operator.curr_system)
     credits = deepcopy(ship_operator.credits)
     waypoint = deepcopy(ship_operator.curr_waypoint)
-    hud = format_base_hud_template(flightMode,system,waypoint,fuel,credits)
+    hud = format_base_hud_template(shipName,flightMode,system,waypoint,fuel,credits)
     cli_print(hud)
 
 #=========
-def print_ship_info(ship_operator:ShipOperator,color:str="white"):
+def print_ship_info():
     string = format_frame_info_template(deepcopy(ship_operator.shipFrame))
     string += format_reactor_info_template(deepcopy(ship_operator.shipReactor))
     string += format_engine_info_template(deepcopy(ship_operator.shipEngine))
-    cli_print(string,color)
+    cli_print(string,color="white")
 
 #=========
-def print_cargo_info(ship_operator:ShipOperator,color:str="white"):
+def print_cargo_info():
     """Print information about ship cargo using stateful information in provided ShipOperator"""
     string = format_cargo_info_template(deepcopy(ship_operator.cargo))
-    cli_print(string,color)
+    cli_print(string,"white")
 
 #=========
-def print_crew_info(ship_operator:ShipOperator,color:str="white"):
+def print_crew_info():
     """Print information about ship cargo using stateful information in provided ShipOperator"""
     string = format_crew_info_template(deepcopy(ship_operator.shipCrew))
-    cli_print(string,color)
+    cli_print(string,color="white")
 
 #=========
-def print_ship_mount_info(ship_operator:ShipOperator,color:str="white"):
+def print_ship_mount_info():
     """Print information about ship mounts using stateful information in provided ShipOperator"""
     mountPoints = ship_operator.shipFrame['mountingPoints']
     string = format_ship_mount_info_template(deepcopy(ship_operator.shipMounts),mountPoints)
-    cli_print(string,color)
+    cli_print(string,color="white")
 
 #=========
-def print_ship_module_info(ship_operator:ShipOperator,color:str="white"):
+def print_ship_module_info():
     """Print information about ship mounts using stateful information in provided ShipOperator"""
-    moduletSlots = ship_operator.shipFrame['moduleSlots']
-    string = format_ship_modules_info_template(deepcopy(ship_operator.shipMounts),moduletSlots)
-    cli_print(string,color)
+    moduleSlots = ship_operator.shipFrame['moduleSlots']
+    string = format_ship_modules_info_template(deepcopy(ship_operator.shipModules),moduleSlots)
+    cli_print(string,color="white")
 
 #==========
-def print_contracts_info(color:str="chartreuse2") -> None:
+def print_contracts_info() -> None:
     """Print information about player contracts"""
     contracts = Contracts()
     data = contracts.list_all_contracts()
-    cli_print(format_contract_list(data),color)
+    cli_print(format_contract_list(data),color="chartreuse2")
+
+#==========
+def print_cargo_and_crew() -> None:
+    print_crew_info()
+    print_cargo_info()
+
+#==========
+def print_all() -> None:
+    print_cargo_and_crew()
+    print_ship_info()
+    print_ship_mount_info()
+    print_ship_module_info()
+
+#==========
+info_menu = {
+    "show ship cargo and crew": {
+        "func": lambda: print_cargo_and_crew(),
+        "desc": "Collect information about resources at your current location."
+    },
+    "show ship mounts": {
+        "func": lambda: print_ship_mount_info(),
+        "desc": "Extract resources from the current location\nwith an optional target resource."
+    },
+    "show ship modules": {
+        "func": lambda: print_ship_module_info(),
+        "desc": "Create a new resource from raw resources in cargo.\nRequires refining module."
+    },
+    "show ship subsystems": {
+        "func": lambda: print_ship_info(),
+        "desc": "Show information about the ship related to mining!"
+    },
+    "show all ship information": {
+        "func": lambda: print_all(),
+        "desc": "Show information about the ship related to mining!"
+    },
+    "cancel": {
+        "func": lambda: None,
+        "desc": "Show information about the ship related to mining!"
+    },
+}
