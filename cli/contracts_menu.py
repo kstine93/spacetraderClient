1dc907de772e4590a10bf6900c873791b85fe6d8
Kevin Stine <kstine93@gmail.com>
2023-07-20 15:31:21 +0200

    Added new 'choose contract' functionality. This also accepts the contract if not already accepted.

--- cli/contracts_menu.py
+++ cli/contracts_menu.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from typing import Callable
-from cli_utilities import cli_print, cli_clear, use_game_menu
+from cli_utilities import *
 from src.contracts import Contracts
 from src.ship_operator import ShipOperator
 from art.str_formatting import (format_contract_list,
@@ -9,6 +9,7 @@ from art.str_formatting import (format_contract_list,
 
 #==========
 ship_operator:ShipOperator
+contracts = Contracts()
 
 #==========
 contracts_menu_color = "chartreuse2" #Color used by default in cli_print
@@ -34,7 +35,6 @@ def contracts_loop(ship:ShipOperator,headerFunc:Callable) -> bool:
 #==========
 def print_contracts_info() -> None:
     """Print information about player contracts"""
-    contracts = Contracts()
     data = contracts.list_all_contracts()
     if len(data) > 0:
         cli_print(format_contract_list(data),color=contracts_menu_color)
@@ -51,17 +51,41 @@ def print_current_contract_info() -> None:
         cli_print("(No contracts yet negotiated)",color=contracts_menu_color)
 
 #==========
-def switch_contract():
-    '''
-    Functionality:
-    1. List contracts
-    2. Player chooses contract they want to pursue
-    3. Player is asked to confirm if they want to accept the contract (IF THEY ACCEPT BUT FAIL TO FULFILL,
-        THERE ARE IN-GAME CONSEQUENCES!). Present Y/N confirmation
-    3. If accept, set this contract as 'pursuedContract' in ship_operator class
-    4. If deny, abort command.
-    '''
-    pass
+def choose_contract():
+    contract_list = contracts.list_all_contracts()
+    chosen_contract = pick_contract_from_menu(contract_list)
+
+    #If contract already accepted:
+    contract = [con for con in contract_list if con['id'] == chosen_contract]
+    if contract[0]['accepted'] == True:
+        ship_operator.set_pursuedContractId(chosen_contract)
+        return
+
+    accept = "Yes, accept this contract."
+    decline = "No, do not accept."
+    menu = create_menu(menu_items=[accept,decline],prompt="Accept this contract? Unfinished contracts can damage your reputation!")
+    resp = menu_prompt(menu)
+
+    if resp == decline:
+        return
+
+    ship_operator.accept_contract(chosen_contract)
+    ship_operator.set_pursuedContractId(chosen_contract)
+
+#==========
+def pick_contract_from_menu(contract_list:list[dict]) -> str:
+    menu_items = []
+    for item in contract_list:
+        preview = format_contract_template(item)
+        preview = preview.replace("|","\|") #For preview in menu, normal bars must be escaped.
+        menu_items.append(f"{item['id']}|{preview}")
+
+    contract_menu = create_menu(menu_items,
+                                "Pick the contract you would like to pursue:",
+                                preview_command=lambda x: x, #print traits to preview box
+                                preview_title="Contract description",
+                                preview_size=0.5)
+    return menu_prompt(contract_menu)
 
 #==========
 def request_new_contract():
@@ -75,8 +99,18 @@ def request_new_contract():
 
 #==========
 def deliver_for_current_contract():
-    #NOTE: This command should also check if the current contract is fulfilled and,
-    # if so, also attempt to 'fulfill' the contract, netting the final pay.
+    '''
+    Functionality:
+    1. Get pursuedContract details.
+    2. Get item(s) that need to be procured from contract details
+        2a. NOTE: This could be just 1 item (it is now), but it could in the future be multiple...
+        2b. NOTE: I don't have any examples of contracts that are NOT acquiring and delivering items,
+            but in the future, this could happen...
+    3. Get 'quantity' of item from cargo
+    4. If quantity more than 0, call 'deliver' function
+    5. Check if total delivered amount exceeds contract requirements
+        5a. If yes, call 'fulfill contract'
+    '''
     pass
 
 #==========
@@ -89,8 +123,8 @@ contracts_menu = {
         "func": lambda: print_current_contract_info(),
         "desc": "Print information about your current ship's cargo and crew."
     },
-    "switch current contract": {
-        "func": lambda: switch_contract(),
+    "choose current contract": {
+        "func": lambda: choose_contract(),
         "desc": "Print information about your current ship's cargo and crew."
     },
     "request new contract": {
